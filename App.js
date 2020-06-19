bb47c9acbb7ac1aa557faa1ab4e88d5b7ad90ab7
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-06-19 22:44:26 +0100

    Temporarily rendering Card only for viewing

--- App.js
+++ App.js
@@ -2,17 +2,22 @@ import React from "react"
 import { View } from "react-native"
 
 import WelcomeScreen from "./app/screens/WelcomeScreen"
+import Card from "./app/components/Card"
 
 export default function App() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
       }}
     >
-      <WelcomeScreen />
+      <Card
+        title="Red jacket for sale"
+        subTitle="£10"
+        image={require("./app/assets/red-jacket.jpg")}
+      />
     </View>
   )
 }
