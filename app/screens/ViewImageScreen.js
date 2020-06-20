d427392b813182c02989350f29472a5b1620fbc8
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-06-20 16:26:58 +0100

    New Setup for ViewImageScreen. Icons included

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,13 +1,22 @@
 import React from "react"
 import { View, Image, StyleSheet } from "react-native"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 
 import colors from "../config/colors"
 
 export default function ViewImageScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon} />
-      <View style={styles.deleteIcon} />
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" color={colors.white} size={35} />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          color={colors.white}
+          size={35}
+        />
+      </View>
       <Image source={require("../assets/chair.jpg")} style={styles.image} />
     </View>
   )
@@ -15,24 +24,18 @@ export default function ViewImageScreen() {
 
 const styles = StyleSheet.create({
   closeIcon: {
-    backgroundColor: colors.primary,
-    height: 50,
     left: 30,
     position: "absolute",
     top: 40,
-    width: 50,
   },
   container: {
     backgroundColor: colors.black,
     flex: 1,
   },
   deleteIcon: {
-    backgroundColor: colors.secondary,
-    height: 50,
     right: 30,
     position: "absolute",
     top: 40,
-    width: 50,
   },
   image: {
     height: "100%",
