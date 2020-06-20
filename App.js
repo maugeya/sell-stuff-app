1560f2d4ecf1aa644a9fd5193f16cfa8432447e8
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-06-20 16:25:12 +0100

    Temporary rendering of ListingDetailsScreen

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import React from "react"
 import { View } from "react-native"
 
-import WelcomeScreen from "./app/screens/WelcomeScreen"
+import ViewImageScreen from "./app/screens/ViewImageScreen"
 import Card from "./app/components/Card"
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen"
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subTitle="£10"
-        image={require("./app/assets/red-jacket.jpg")}
-      />
-    </View>
-  )
+  return <ListingDetailsScreen />
 }
