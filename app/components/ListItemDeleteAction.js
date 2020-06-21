45e7d59256929fe275fd76623cce4f17032d5583
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-06-21 21:02:24 +0100

    Adding ability to delete a message from the list

--- app/components/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction.js
@@ -3,18 +3,26 @@ import { View, StyleSheet } from "react-native"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 
 import colors from "../config/colors"
+import { TouchableWithoutFeedback } from "react-native-gesture-handler"
 
-export default function ListItemDeleteAction(props) {
+export default function ListItemDeleteAction({ onPress }) {
   return (
-    <View style={styles.container}>
-      <MaterialCommunityIcons name="trash-can" size={35} color={colors.white} />
-    </View>
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="trash-can"
+          size={35}
+          color={colors.white}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.danger,
+    height: "100%",
     width: 70,
     justifyContent: "center",
     alignItems: "center",
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { FlatList, StyleSheet, View } from "react-native"
 
 import ListItem from "../components/ListItem"
@@ -6,7 +6,7 @@ import Screen from "../components/Screen"
 import ListItemSeparator from "../components/ListItemSeparator"
 import ListItemDeleteAction from "../components/ListItemDeleteAction"
 
-const messages = [
+const initialMessages = [
   {
     id: 1,
     title: "T1",
@@ -21,7 +21,13 @@ const messages = [
   },
 ]
 
-export default function MessageScreen(props) {
+export default function MessageScreen() {
+  const [messages, setMessages] = useState(initialMessages)
+
+  const handleDelete = (message) => {
+    setMessages(messages.filter((m) => m.id !== message.id))
+  }
+
   return (
     <Screen>
       <FlatList
@@ -29,11 +35,12 @@ export default function MessageScreen(props) {
         keyExtractor={(message) => message.id.toString()}
         renderItem={({ item }) => (
           <ListItem
-            renderRightActions={() => <ListItemDeleteAction />}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
             title={item.title}
             subTitle={item.description}
             image={item.image}
-            onPress={() => console.log("Message selected ", item)}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
