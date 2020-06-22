522764ab8702e5c2facb5329351dcc7768b341ce
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-06-22 20:38:45 +0100

    Temporary rendering of AccountScreen for testing

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { View } from "react-native"
 
 import ViewImageScreen from "./app/screens/ViewImageScreen"
 import Card from "./app/components/Card"
+import ListItem from "./app/components/ListItem"
+import MenuIcon from "./app/components/MenuIcon"
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen"
 import MessagesScreen from "./app/screens/MessagesScreen"
+import AccountScreen from "./app/screens/AccountScreen"
 
 export default function App() {
-  return <MessagesScreen />
+  return <AccountScreen />
 }
