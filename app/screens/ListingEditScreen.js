47563452157938296c75e32e5f1a931ee9f6eb6c
Angela Maugey <a.maugey@gmail.com>
2020-06-24 19:10:22 +0100

    Updating imports to come from index file. Adding new AppFormPicker component

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,28 +1,58 @@
 import React from "react"
 import { StyleSheet } from "react-native"
+import * as Yup from "yup"
 
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+} from "../components/forms"
 import Screen from "../components/Screen"
-import AppForm from "../components/forms/AppForm"
-import AppFormField from "../components/forms/AppFormField"
-import SubmitButton from "../components/forms/SubmitButton"
 
-export default function ListingEditScreen() {
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(1).label("Title"),
+  price: Yup.number().required().min(1).max(10000).label("Price"),
+  description: Yup.string().label("Description"),
+  category: Yup.object().required().nullable().label("Category"),
+})
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Kitchenware", value: 3 },
+]
+
+function ListingEditScreen() {
   return (
     <Screen style={styles.container}>
       <AppForm
-        initialValues={{ title: "", price: "", description: "" }}
+        initialValues={{
+          title: "",
+          price: "",
+          description: "",
+          category: null,
+        }}
         onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
       >
-        <AppFormField autoCorrect={false} name="title" placeholder="Title" />
+        <AppFormField maxLength={255} name="title" placeholder="Title" />
         <AppFormField
-          autoCorrect={false}
-          name="price"
           keyboardType="numeric"
-          placeholder="Price (£)"
+          maxLength={8}
+          name="price"
+          placeholder="Price"
+        />
+        <AppFormPicker
+          items={categories}
+          name="category"
+          placeholder="Category"
         />
         <AppFormField
-          autoCorrect={false}
+          maxLength={255}
+          multiline
           name="description"
+          numberOfLines={3}
           placeholder="Description"
         />
         <SubmitButton title="Post" />
@@ -36,3 +66,4 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 })
+export default ListingEditScreen
