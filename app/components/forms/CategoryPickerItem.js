e8bbaa953573dd7dc23d86e329d3b4ea1378d90a
Angela Maugey <a.maugey@gmail.com>
2020-06-28 14:03:25 +0100

    Updating CategoryPickerItem to show different categories and icons

--- app/components/forms/CategoryPickerItem.js
+++ app/components/forms/CategoryPickerItem.js
@@ -1,5 +1,31 @@
 import React from "react"
+import { View, StyleSheet } from "react-native"
 
-export default function CategoryPickerItem() {
-  return null
+import AppText from "../AppText"
+import MenuIcon from "../MenuIcon"
+
+export default function CategoryPickerItem({ item, onPress }) {
+  return (
+    <View style={styles.container}>
+      <MenuIcon
+        backgroundColor={item.backgroundColor}
+        name={item.icon}
+        size={80}
+      />
+      <AppText style={styles.label}>{item.label}</AppText>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    width: "33%",
+  },
+  label: {
+    marginTop: 5,
+    textAlign: "center",
+  },
+})
