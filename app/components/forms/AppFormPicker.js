1ef8e7a8067bf12dc192c233f1455af24e020854
Angela Maugey <a.maugey@gmail.com>
2020-06-28 10:16:54 +0100

    Allowing AppFormPicker to accept a prop determining which type of PicketItem is to be used

--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -4,13 +4,20 @@ import { useFormikContext } from "formik"
 import AppPicker from "./AppPicker"
 import ErrorMessage from "./ErrorMessage"
 
-export default function AppFormPicker({ items, name, placeholder, width }) {
+export default function AppFormPicker({
+  items,
+  name,
+  PickerItemComponent,
+  placeholder,
+  width,
+}) {
   const { setFieldValue, errors, touched, values } = useFormikContext()
   return (
     <>
       <AppPicker
         items={items}
         onSelectItem={(item) => setFieldValue(name, item)}
+        PickerItemComponent={PickerItemComponent}
         placeholder={placeholder}
         selectedItem={values[name]}
         width={width}
