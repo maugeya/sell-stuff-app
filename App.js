4b948b4577f2685ccf0b2c25a407af9cbfbb202c
Angela Maugey <a.maugey@gmail.com>
2020-06-29 21:32:12 +0100

    Temporary rendering of ImageInput for testing purposes

--- App.js
+++ App.js
@@ -1,42 +1,17 @@
 import React, { useState } from "react"
-import { View, TextInput, Text, Switch } from "react-native"
 
-import ViewImageScreen from "./app/screens/ViewImageScreen"
 import Screen from "./app/components/Screen"
-import AppTextInput from "./app/components/AppTextInput"
-import AppPicker from "./app/components/AppPicker"
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen"
-import MessagesScreen from "./app/screens/MessagesScreen"
-import AccountScreen from "./app/screens/AccountScreen"
-import ListingsScreen from "./app/screens/ListingsScreen"
-
-const categories = [
-  {
-    label: "Furniture",
-    value: 1,
-  },
-  {
-    label: "Clothing",
-    value: 2,
-  },
-  {
-    label: "KitchenWare",
-    value: 3,
-  },
-]
+import ImageInput from "./app/components/ImageInput"
 
 export default function App() {
-  const [category, setCategory] = useState()
+  const [imageUri, setImageUri] = useState()
+
   return (
     <Screen>
-      <AppPicker
-        selectedItem={category}
-        items={categories}
-        onSelectItem={(item) => setCategory(item)}
-        icon="apps"
-        placeholder="Category"
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
       />
-      <AppTextInput icon="email" placeholder="email" />
     </Screen>
   )
 }
