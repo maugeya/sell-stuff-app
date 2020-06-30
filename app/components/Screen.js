5d1dd89623b687e751fa230216798d69996395b8
Angela Maugey <a.maugey@gmail.com>
2020-06-30 12:55:27 +0100

    Updating component to properly display padding

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,14 +1,22 @@
 import React from "react"
 import Constants from "expo-constants"
-import { StyleSheet, SafeAreaView, YellowBox } from "react-native"
+import { StyleSheet, SafeAreaView, View } from "react-native"
 
 export default function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>
+  console.log(style)
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={[styles.view, style]}>{children}</View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     paddingTop: Constants.statusBarHeight,
+  },
+  view: {
+    flex: 1,
   },
 })
