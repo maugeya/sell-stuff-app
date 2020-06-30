2b953a749f9020da0686367817b760a3ebe75bb3
Angela Maugey <a.maugey@gmail.com>
2020-06-30 09:55:57 +0100

    Updating ListinEditScreen validation schema and initial values to be aware of images input

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -10,12 +10,14 @@ import {
 } from "../components/forms"
 import Screen from "../components/Screen"
 import CategoryPickerItem from "../components/forms/CategoryPickerItem"
+import AppFormImagePicker from "../components/forms/AppFormImagePicker"
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image"),
 })
 
 const categories = [
@@ -74,10 +76,12 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
+          images: [],
         }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
+        <AppFormImagePicker name="images"></AppFormImagePicker>
         <AppFormField maxLength={255} name="title" placeholder="Title" />
         <AppFormField
           keyboardType="numeric"
