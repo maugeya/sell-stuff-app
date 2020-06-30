37ae164a60f3c8d0b788ce3bb933e0dd797c983b
Angela Maugey <a.maugey@gmail.com>
2020-06-30 21:54:41 +0100

    Using modal effect to allow movement between screens

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -7,8 +7,12 @@ import ListingDetailsScreen from "../screens/ListingDetailsScreen"
 const Stack = createStackNavigator()
 
 export default FeedNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator mode="modal">
     <Stack.Screen name="Listings" component={ListingsScreen} />
-    <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
+    <Stack.Screen
+      name="ListingDetails"
+      component={ListingDetailsScreen}
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 )
