6d92d98d8baa87198530a4ee68b2bdb1bf3a83b2
Angela Maugey <a.maugey@gmail.com>
2020-06-30 22:00:44 +0100

    Making use of AccountNavigation component

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -3,8 +3,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 import ListingsScreen from "../screens/ListingsScreen"
 import ListingEditScreen from "../screens/ListingEditScreen"
-import AccountScreen from "../screens/AccountScreen"
 import FeedNavigator from "./FeedNavigator"
+import AccountNavigator from "./AccountNavigator"
 
 const Tab = createBottomTabNavigator()
 
@@ -12,6 +12,6 @@ export default AppNavigator = () => (
   <Tab.Navigator>
     <Tab.Screen name="Feed" component={FeedNavigator} />
     <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountScreen} />
+    <Tab.Screen name="Account" component={AccountNavigator} />
   </Tab.Navigator>
 )
