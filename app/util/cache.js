3c521ac1b2e8e1362b2bffb8d2b888bc36163f62
Angela Maugey <a.maugey@gmail.com>
2020-07-05 19:44:17 +0100

    Creating cache layer. Creating store for saving data as cache. Creating get function for returning cache data after determining if it is expired

---
+++ app/util/cache.js
@@ -0,0 +1,46 @@
+import { AsyncStorage } from "react-native"
+import moment from "moment"
+
+const prefix = "cache"
+const expiryInMinutes = 5
+
+const isExpired = (item) => {
+  const now = moment(Data.now())
+  const storedTime = moment(item.timestamp)
+  return now.diff(storedTime, "minutes") > expiryInMinutes
+}
+
+const store = async (key, value) => {
+  try {
+    const item = {
+      value,
+      timestamp: Date.now(),
+    }
+    await AsyncStorage.setItem(prefix + key, JSON.stringify(item))
+  } catch (error) {
+    console.log("caching error", error)
+  }
+}
+
+const get = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(prefox + key)
+    const item = JSON.parse(value)
+
+    if (!item) return null
+
+    if (isExpired(item)) {
+      await AsyncStorage.removeItem(prefix + key)
+      return null
+    }
+
+    return item.value
+  } catch (error) {
+    console.log("getting cache error", error)
+  }
+}
+
+export default {
+  get,
+  store,
+}
