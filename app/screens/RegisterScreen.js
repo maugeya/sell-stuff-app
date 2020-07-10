bf60a20d0026292e93e5917003d7d91490660b21
Angela Maugey <a.maugey@gmail.com>
2020-07-10 23:04:08 +0100

    Register user, show error if result is not ok. Auto login of user if registration is successful

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import { StyleSheet } from "react-native"
 import * as Yup from "yup"
 
 import Screen from "../components/Screen"
-import { AppForm, AppFormField, SubmitButton } from "../components/forms"
+import {
+  AppForm,
+  AppFormField,
+  ErrorMessage,
+  SubmitButton,
+} from "../components/forms"
+import usersApi from "../api/users"
+import authApi from "../api/auth"
+import useAuth from "../hooks/useAuth"
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().lowercase().label("Name"),
@@ -11,14 +19,38 @@ const validationSchema = Yup.object().shape({
   password: Yup.string().required().min(4).label("Password"),
 })
 
-export default function RegisterScreen() {
+export default function RegisterScreen({ navigation }) {
+  const [error, setError] = useState()
+  const { login } = useAuth()
+
+  const handleSubmit = async (userInfo) => {
+    const result = await usersApi.register(userInfo)
+
+    if (!result.ok) {
+      if (result.data) setError(result.data.error)
+      else {
+        setError("An unexpected error occurred.")
+        console.log("Register error", result)
+      }
+      return
+    }
+
+    const { data: authToken } = await authApi.login(
+      userInfo.email,
+      userInfo.password
+    )
+
+    login(authToken)
+  }
+
   return (
     <Screen style={styles.container}>
       <AppForm
         initialValues={{ name: "", email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
+        <ErrorMessage error={error} visible={error} />
         <AppFormField
           autoCorrect={false}
           icon="account"
